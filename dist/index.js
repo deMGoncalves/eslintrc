2091104f60ce0d3b02b1e2f5247564d8108cdb28
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-02-23 10:26:48 -0300

    build(dist): automatic new publish

---
+++ dist/index.js
@@ -0,0 +1,34 @@
+"use strict";
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true,
+        "jest/globals": true
+    },
+    "extends": [
+        "standard",
+        "plugin:jest/recommended"
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaVersion": 12,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "@typescript-eslint",
+        "jest"
+    ],
+    "rules": {
+        "jest/no-disabled-tests": "warn",
+        "jest/no-focused-tests": "error",
+        "jest/no-identical-title": "error",
+        "jest/prefer-to-have-length": "warn",
+        "jest/valid-expect": "error",
+        'no-sequences': 'off'
+    },
+    "settings": {
+        "jest": {
+            "version": 26
+        }
+    }
+};
