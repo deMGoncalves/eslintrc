da6ab7842258e5733194fc3fe0a9691a380f2da1
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-04-05 15:00:57 -0300

    build(dist): automatic new publish

--- dist/index.js
+++ dist/index.js
@@ -27,6 +27,7 @@ module.exports = {
         'jest/no-identical-title': 'error',
         'jest/prefer-to-have-length': 'warn',
         'jest/valid-expect': 'error',
+        'no-return-assign': 'off',
         'no-sequences': 'off',
         'no-unused-expressions': 'off',
         'no-unused-vars': [
